7c9b30fa556fabdf7b232b3c880911053c075300
Kevin <dkevin04@vt.edu>
2025-04-13 16:41:38 -0400

    Main now reads bin file and sorts
    Prints a sorted byte array read from the binary file to the console as base 10 ints

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /*
  * A quicksort implementation which takes text files as input assuming each data
@@ -48,46 +51,22 @@ public class Quicksort {
          */
         String inputFile = args[0];
         String outputFile = args[2];
-
-        try (InputStream inputStream = new FileInputStream(inputFile);
-            OutputStream outputStream = new FileOutputStream(outputFile);) {
-            int byteRead = -1;
-
-            while ((byteRead = inputStream.read()) != -1) {
-                ;
-                /*
-                 * Must write to output
-                 * 
-                 * name of the data file
-                 * 
-                 * The number of cache hits, or times your program found the
-                 * data it needed in a buffer and did not have to go to the disk
-                 * 
-                 * The number of disk reads, or times your program had to read a
-                 * block of data from disk into a buffer
-                 * 
-                 * The number of disk writes, or times your program had to write
-                 * a block of data to disk from a buffer
-                 * 
-                 * The time that your program took to execute the Quicksort. Put
-                 * two calls to the standard Java timing method
-                 * System.currentTimeMillis() in your program, before and after
-                 * you call the sort function. This method returns a long value.
-                 * The difference between the two values will be the total
-                 * runtime in milliseconds. You should ONLY time the sorting,
-                 * and not the time to write the program output as described
-                 * above
-                 */
-            }
-
+        byte[] fileContents = new byte[1024];
+        try (RandomAccessFile stmt = new RandomAccessFile(inputFile, "rw")) {
+            stmt.read(fileContents);
+            quickSort(fileContents, 0, 1023);
+            stmt.close();
         }
-        catch (IOException ex) {
-            ex.printStackTrace();
+        catch (IOException e) {
+            // Exception handling
+        }
+        for (int i = 0; i < fileContents.length; i++) {
+            System.out.print(fileContents[i] + " ");
         }
     }
 
 
-    public void quickSort(char[] arr, int begin, int end) {
+    public static void quickSort(byte[] arr, int begin, int end) {
         if (begin < end) {
             int partitionIndex = partition(arr, begin, end);
 
@@ -97,21 +76,21 @@ public class Quicksort {
     }
 
 
-    public int partition(char[] arr, int begin, int end) {
-        int pivot = arr[end];
+    public static int partition(byte[] arr, int begin, int end) {
+        byte pivot = arr[end];
         int i = (begin - 1);
 
         for (int j = begin; j < end; j++) {
             if (arr[j] <= pivot) {
                 i++;
 
-                char swapTemp = arr[i];
+                byte swapTemp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = swapTemp;
             }
         }
 
-        char swapTemp = arr[i + 1];
+        byte swapTemp = arr[i + 1];
         arr[i + 1] = arr[end];
         arr[end] = swapTemp;
 
