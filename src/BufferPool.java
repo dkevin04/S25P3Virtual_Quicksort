d1dc8e044df23c3f3830c7ffcfd304bc3288fb9e
Blakeeverman <blakee@vt.edu>
2025-04-21 19:40:28 -0400

    fixed the bugs when not working with more than 1 buffers

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -30,190 +30,186 @@ import java.io.*;
 // during the discussion. I have violated neither the spirit nor
 // letter of this restriction.
 public class BufferPool {
-    private static final int BLOCK_SIZE = 4096;
-    private Buffer[] buffers;
-    private int numBuffers;
-    private int size;
-    private RandomAccessFile raf;
-    private Statistics stat;
+	private static final int BLOCK_SIZE = 4096;
+	private Buffer[] buffers;
+	private int numBuffers;
+	private int size;
+	private RandomAccessFile raf;
+	private Statistics stat;
 
-    /**
-     * Constructs a BufferPool object
-     * 
-     * @param file
-     *            the file that stores the data being read in
-     * @param num
-     *            number of buffer arrays to be created
-     * @param stat
-     *            stat object so track efficiency metrics
-     * @throws FileNotFoundException
-     *             throw if the file does not exist in the library
-     */
-    public BufferPool(String file, int num, Statistics stat)
-        throws FileNotFoundException {
-        this.raf = new RandomAccessFile(file, "rw");
-        this.numBuffers = num;
-        this.buffers = new Buffer[num];
-        this.stat = stat;
+	/**
+	 * Constructs a BufferPool object
+	 * 
+	 * @param file the file that stores the data being read in
+	 * @param num  number of buffer arrays to be created
+	 * @param stat stat object so track efficiency metrics
+	 * @throws FileNotFoundException throw if the file does not exist in the library
+	 */
+	public BufferPool(String file, int num, Statistics stat) throws FileNotFoundException {
+		this.raf = new RandomAccessFile(file, "rw");
+		this.numBuffers = num;
+		this.buffers = new Buffer[num];
+		this.stat = stat;
 
-    }
+	}
 
+	/**
+	 * This method checks all of the buffers in the pool to see if the wanted buffer
+	 * is in there already, if it is then we copy the contents into the destination
+	 * "space." If not, then we either add the buffer to the pool, or evict the
+	 * least recently used buffer. Either way, we shift down the buffers so that the
+	 * buffer we were searching for is now at the front of the pool.
+	 * 
+	 * @param space the byte array which will hold the 4 byte record
+	 * @param sz    size of the record being read, here assumed to be 4 as the data
+	 *              in the file is 4 byte records
+	 * @param pos   position of the beginning of the 4 byte to be read
+	 * @throws IOException throws upon failure to acquire or write i/o sufficiently
+	 */
+	public void getbytes(byte[] space, int sz, int pos) throws IOException {
+		int block = pos / BLOCK_SIZE;
+		int start = pos % BLOCK_SIZE;
 
-    /**
-     * This method checks all of the buffers in the pool to see if the wanted
-     * buffer is in there already, if it is then we copy the contents into the
-     * destination "space." If not, then we either add the buffer to the pool,
-     * or evict the least recently used buffer. Either way, we shift down the
-     * buffers so that the buffer we were searching for is now at the front of
-     * the pool.
-     * 
-     * @param space
-     *            the byte array which will hold the 4 byte record
-     * @param sz
-     *            size of the record being read, here assumed to be 4 as the
-     *            data in the file is 4 byte records
-     * @param pos
-     *            position of the beginning of the 4 byte to be read
-     * @throws IOException
-     *             throws upon failure to acquire or write i/o sufficiently
-     */
-    public void getbytes(byte[] space, int sz, int pos) throws IOException {
-        int block = pos / BLOCK_SIZE;
-        int start = pos % BLOCK_SIZE;
+		/*
+		 * this is code for when we have a cache hit
+		 */
+		for (int i = 0; i < size; i++) {
+			if (buffers[i] != null && buffers[i].getBlockIndex() == block) {
+				Buffer temp = buffers[i];
+				for (int j = i; j > 0; j--) {
+					buffers[j] = buffers[j - 1];
+				}
+				buffers[0] = temp;
+				stat.increaseHits();
+				System.arraycopy(temp.getData(), start, space, 0, sz);
+				return;
+			}
+		}
+		/*
+		 * when we have a cache miss and have to load
+		 */
+		byte[] data = new byte[BLOCK_SIZE];
+		raf.seek(block * BLOCK_SIZE);
+		raf.readFully(data);
+		stat.increaseReads();
+		Buffer newBuff = new Buffer(block, data);
 
-        /*
-         * this is code for when we have a cache hit
-         */
-        for (int i = 0; i < size; i++) {
-            if (buffers[i] != null && buffers[i].getBlockIndex() == block) {
-                System.arraycopy(buffers, 0, buffers, 1, i);
-                buffers[0] = buffers[i];
-                stat.increaseHits();
-                System.arraycopy(buffers[i].getData(), start, space, 0, sz);
-                return;
-            }
-        }
-        /*
-         * when we have a cache miss and have to load
-         */
-        byte[] data = new byte[BLOCK_SIZE];
-        raf.seek(block * BLOCK_SIZE);
-        raf.readFully(data);
-        stat.increaseReads();
-        Buffer newBuff = new Buffer(block, data);
+		/*
+		 * buffer pool full
+		 */
+		if (numBuffers == size) {
+			Buffer evicted = buffers[numBuffers - 1];
+			if (evicted.isDirty()) {
+				raf.seek(evicted.getBlockIndex() * BLOCK_SIZE);
+				raf.write(evicted.getData());
+				stat.increaseWrites();
+				evicted.setDirty(false);
+			}
+			for (int j = numBuffers - 1; j > 0; j--) {
+				buffers[j] = buffers[j - 1];
+			}
+			buffers[0] = newBuff;
+		}
+		/*
+		 * not full
+		 */
+		else {
+			for (int j = size; j > 0; j--) {
+				buffers[j] = buffers[j - 1];
+			}
+			buffers[0] = newBuff;
+			size++;
+		}
+		System.arraycopy(data, start, space, 0, sz);
 
-        /*
-         * buffer pool full
-         */
-        if (numBuffers == size) {
-            Buffer evicted = buffers[numBuffers - 1];
-            if (evicted.isDirty()) {
-                raf.seek(evicted.getBlockIndex() * BLOCK_SIZE);
-                raf.write(evicted.getData());
-                stat.increaseWrites();
-                evicted.setDirty(false);
-            }
-            System.arraycopy(buffers, 0, buffers, 1, numBuffers - 1);
-            buffers[0] = newBuff;
-        }
-        /*
-         * not full
-         */
-        else {
-            System.arraycopy(buffers, 0, buffers, 1, size);
-            buffers[0] = newBuff;
-            size++;
-        }
-        System.arraycopy(data, start, space, 0, sz);
+	}
 
-    }
+	/**
+	 * Inserts the passed byte array space at the specificity position
+	 * 
+	 * @param space the byte array which will hold the 4 byte record
+	 * @param sz    size of the record being inserted, here assumed to be 4 as the
+	 *              data in the file is 4 byte records
+	 * @param pos   position of the beginning of the 4 byte to be edited
+	 * @throws IOException throws upon failure to acquire or write i/o sufficiently
+	 */
+	public void insert(byte[] space, int sz, int pos) throws IOException {
+		int block = pos / BLOCK_SIZE;
+		int start = pos % BLOCK_SIZE;
 
+		/*
+		 * this is code for when we have a cache hit
+		 */
+		for (int i = 0; i < size; i++) {
+			if (buffers[i] != null && buffers[i].getBlockIndex() == block) {
+				Buffer temp = buffers[i];
+				for (int j = i; j > 0; j--) {
+					buffers[j] = buffers[j - 1];
+				}
+				buffers[0] = temp;
+				stat.increaseHits();
+				System.arraycopy(space, 0, temp.getData(), start, sz);
+				temp.setDirty(true);
+				return;
+			}
+		}
+		/*
+		 * when we have a cache miss and have to load
+		 */
+		byte[] data = new byte[BLOCK_SIZE];
+		raf.seek(block * BLOCK_SIZE);
+		raf.readFully(data);
+		stat.increaseReads();
+		Buffer newBuff = new Buffer(block, data);
 
-    /**
-     * Inserts the passed byte array space at the specificity position
-     * 
-     * @param space
-     *            the byte array which will hold the 4 byte record
-     * @param sz
-     *            size of the record being inserted, here assumed to be 4 as the
-     *            data in the file is 4 byte records
-     * @param pos
-     *            position of the beginning of the 4 byte to be edited
-     * @throws IOException
-     *             throws upon failure to acquire or write i/o sufficiently
-     */
-    public void insert(byte[] space, int sz, int pos) throws IOException {
-        int block = pos / BLOCK_SIZE;
-        int start = pos % BLOCK_SIZE;
+		/*
+		 * buffer pool full
+		 */
+		if (numBuffers == size) {
+			Buffer evicted = buffers[numBuffers - 1];
+			if (evicted.isDirty()) {
+				raf.seek(evicted.getBlockIndex() * BLOCK_SIZE);
+				raf.write(evicted.getData());
+				stat.increaseWrites();
+				evicted.setDirty(false);
+			}
+			for (int j = numBuffers - 1; j > 0; j--) {
+				buffers[j] = buffers[j - 1];
+			}
+			buffers[0] = newBuff;
+		}
+		/*
+		 * not full
+		 */
+		else {
+			for (int j = size; j > 0; j--) {
+				buffers[j] = buffers[j - 1];
+			}
+			buffers[0] = newBuff;
+			size++;
+		}
+		System.arraycopy(space, 0, newBuff.getData(), start, sz);
+		newBuff.setDirty(true);
 
-        /*
-         * this is code for when we have a cache hit
-         */
-        for (int i = 0; i < size; i++) {
-            if (buffers[i] != null && buffers[i].getBlockIndex() == block) {
-                System.arraycopy(buffers, 0, buffers, 1, i);
-                buffers[0] = buffers[i];
-                stat.increaseHits();
-                System.arraycopy(space, 0, buffers[i].getData(), start, sz);
-                buffers[0].setDirty(true);
-                return;
-            }
-        }
-        /*
-         * when we have a cache miss and have to load
-         */
-        byte[] data = new byte[BLOCK_SIZE];
-        raf.seek(block * BLOCK_SIZE);
-        raf.readFully(data);
-        stat.increaseReads();
-        Buffer newBuff = new Buffer(block, data);
+	}
 
-        /*
-         * buffer pool full
-         */
-        if (numBuffers == size) {
-            Buffer evicted = buffers[numBuffers - 1];
-            if (evicted.isDirty()) {
-                raf.seek(evicted.getBlockIndex() * BLOCK_SIZE);
-                raf.write(evicted.getData());
-                stat.increaseWrites();
-                evicted.setDirty(false);
-            }
-            System.arraycopy(buffers, 0, buffers, 1, numBuffers - 1);
-            buffers[0] = newBuff;
-        }
-        /*
-         * not full
-         */
-        else {
-            System.arraycopy(buffers, 0, buffers, 1, size);
-            buffers[0] = newBuff;
-            size++;
-        }
-        System.arraycopy(space, 0, newBuff.getData(), start, sz);
-        newBuff.setDirty(true);
+	/**
+	 * We find the dirty blocks, go to it and write out its entire block to disk. We
+	 * make sure to set dirt to false at the end.
+	 * 
+	 * @throws IOException throws upon failure to acquire or write i/o sufficiently
+	 */
+	public void flush() throws IOException {
+		for (int i = 0; i < size; i++) {
+			Buffer buffer = buffers[i];
+			if (buffer.isDirty() && buffer != null) {
+				int start = buffer.getBlockIndex() * BLOCK_SIZE;
+				raf.seek(start);
+				raf.write(buffer.getData());
+				stat.increaseWrites();
+				buffer.setDirty(false);
+			}
+		}
 
-    }
-
-
-    /**
-     * We find the dirty blocks, go to it and write out its entire block to
-     * disk. We make sure to set dirt to false at the end.
-     * 
-     * @throws IOException
-     *             throws upon failure to acquire or write i/o sufficiently
-     */
-    public void flush() throws IOException {
-        for (int i = 0; i < numBuffers; i++) {
-            Buffer buffer = buffers[i];
-            if (buffer.isDirty() && buffer != null) {
-                int start = buffer.getBlockIndex() * BLOCK_SIZE;
-                raf.seek(start);
-                raf.write(buffer.getData());
-                stat.increaseWrites();
-                buffer.setDirty(false);
-            }
-        }
-
-    }
+	}
 }
--- src/Quicksort.java
+++ src/Quicksort.java
@@ -30,148 +30,133 @@ import java.nio.ByteBuffer;
 // letter of this restriction.
 
 public class Quicksort {
-    /**
-     * @param args
-     *            Command line parameters. See the project spec!!!
-     */
-    public static void main(String[] args) {
+	/**
+	 * @param args Command line parameters. See the project spec!!!
+	 */
+	public static void main(String[] args) {
 
-        // Checks for proper arguments to be passed to avoid null pointer and
-        // i/o errors
-        if (args.length != 3) {
-            System.out.println("Wrong number of command line args, "
-                + "please pass 2 files and the numbers of buffers");
-        }
-        if (Integer.valueOf(args[1]).intValue() > 20 && Integer.valueOf(args[1])
-            .intValue() < 1) {
-            System.out.println("Wrong number of buffers, "
-                + "please input an integer between 1 and 20");
-        }
+		// Checks for proper arguments to be passed to avoid null pointer and
+		// i/o errors
+		if (args.length != 3) {
+			System.out
+					.println("Wrong number of command line args, " + "please pass 2 files and the numbers of buffers");
+		}
+		if (Integer.valueOf(args[1]).intValue() > 20 && Integer.valueOf(args[1]).intValue() < 1) {
+			System.out.println("Wrong number of buffers, " + "please input an integer between 1 and 20");
+		}
 
-        // variables used to access i/o files, construct the BufferPool, and a
-        // stat object to track important data
-        String inputFile = args[0];
-        int numBuffers = Integer.valueOf(args[1]);
-        String outputFile = args[2];
-        BufferPool data = null;
-        Statistics stat = null;
+		// variables used to access i/o files, construct the BufferPool, and a
+		// stat object to track important data
+		String inputFile = args[0];
+		int numBuffers = Integer.valueOf(args[1]);
+		String outputFile = args[2];
+		BufferPool data = null;
+		Statistics stat = null;
 
-        // this try catch block opens the input data file and begins the process
-        // of sorting data and processing data to be written to the stat file
-        try (RandomAccessFile stmt = new RandomAccessFile(inputFile, "rw")) {
+		// this try catch block opens the input data file and begins the process
+		// of sorting data and processing data to be written to the stat file
+		try (RandomAccessFile stmt = new RandomAccessFile(inputFile, "rw")) {
 
-            // initializes two variables, stat to track performance data and
-            // data to interface with the large input file to avoid excessive
-            // memory calls
-            stat = new Statistics(inputFile);
-            data = new BufferPool(inputFile, numBuffers, stat);
+			// initializes two variables, stat to track performance data and
+			// data to interface with the large input file to avoid excessive
+			// memory calls
+			stat = new Statistics(inputFile);
+			data = new BufferPool(inputFile, numBuffers, stat);
 
-            int numRecords = (int)stmt.length() / 4;
+			int numRecords = (int) stmt.length() / 4;
 
-            // starts the clock to measure the time it takes to sort
-            long start = System.currentTimeMillis();
-            // sorting here
-            quickSort(data, 0, numRecords - 1);
-            // end of the sorting process, time to execute measured in
-            // milliseconds
-            long end = System.currentTimeMillis();
+			// starts the clock to measure the time it takes to sort
+			long start = System.currentTimeMillis();
+			// sorting here
+			quickSort(data, 0, numRecords - 1);
+			// end of the sorting process, time to execute measured in
+			// milliseconds
+			long end = System.currentTimeMillis();
 
-            // rewrites the inputfile's data to now be fully sorted
-            data.flush();
+			// rewrites the inputfile's data to now be fully sorted
+			data.flush();
 
-            // Calculates sorting time, passes it to the stat object and now
-            // write the performance data to the stat file
-            stat.setExTime(end - start);
-            stat.writeToFile(outputFile);
-        }
+			// Calculates sorting time, passes it to the stat object and now
+			// write the performance data to the stat file
+			stat.setExTime(end - start);
+			stat.writeToFile(outputFile);
+		}
 
-        // if file is not present or something else in i/o has gone very wrong
-        catch (IOException e) {
-            // Exception handling
-            e.printStackTrace();
-        }
-    }
+		// if file is not present or something else in i/o has gone very wrong
+		catch (IOException e) {
+			// Exception handling
+			e.printStackTrace();
+		}
+	}
 
+	/**
+	 * QuickSort implementation using an external sorting paradigm so sort large
+	 * files of data. Note that the current implementation assumes the data is
+	 * readable to a byte format
+	 * 
+	 * @param bp    bufferpool object which stores the file's data
+	 * @param begin first index of the unsorted portion of array
+	 * @param end   last index of the unsorted portion of array
+	 * @throws IOException throws upon failure to acquire or write i/o sufficiently
+	 */
+	public static void quickSort(BufferPool bp, int begin, int end) throws IOException {
+		if (begin < end) {
+			// creates a pivot to bisect array and now gives the index of that
+			// pivot so array can be bisected here in quickSort
+			int partitionIndex = partition(bp, begin, end);
 
-    /**
-     * QuickSort implementation using an external sorting paradigm so sort large
-     * files of data. Note that the current implementation assumes the data is
-     * readable to a byte format
-     * 
-     * @param bp
-     *            bufferpool object which stores the file's data
-     * @param begin
-     *            first index of the unsorted portion of array
-     * @param end
-     *            last index of the unsorted portion of array
-     * @throws IOException
-     *             throws upon failure to acquire or write i/o sufficiently
-     */
-    public static void quickSort(BufferPool bp, int begin, int end)
-        throws IOException {
-        if (begin < end) {
-            // creates a pivot to bisect array and now gives the index of that
-            // pivot so array can be bisected here in quickSort
-            int partitionIndex = partition(bp, begin, end);
+			// begins sorting the left half of the array
+			quickSort(bp, begin, partitionIndex - 1);
+			// begins sorting the right half of the array
+			quickSort(bp, partitionIndex + 1, end);
+		}
+	}
 
-            // begins sorting the left half of the array
-            quickSort(bp, begin, partitionIndex - 1);
-            // begins sorting the right half of the array
-            quickSort(bp, partitionIndex + 1, end);
-        }
-    }
+	/**
+	 * Takes a value as the pivot and places the rest of the array left or right
+	 * relative to the pivot. Returns the index of the pivot
+	 * 
+	 * @param bp    bufferpool object which stores the file's data
+	 * @param begin first index of the unsorted portion of array
+	 * @param end   last index of the unsorted portion of array
+	 * @return index of the pivot
+	 * @throws IOException throws upon failure to acquire or write i/o sufficiently
+	 */
+	private static int partition(BufferPool bp, int begin, int end) throws IOException {
+		// variables to store data that is being swapped
+		byte[] pivot = new byte[4];
+		byte[] left = new byte[4];
+		byte[] right = new byte[4];
 
+		// loads the 4 byte record as a pivot used to sort the array
+		bp.getbytes(pivot, 4, end * 4);
+		short pivotKey = ByteBuffer.wrap(pivot).getShort();
 
-    /**
-     * Takes a value as the pivot and places the rest of the array left or right
-     * relative to the pivot. Returns the index of the pivot
-     * 
-     * @param bp
-     *            bufferpool object which stores the file's data
-     * @param begin
-     *            first index of the unsorted portion of array
-     * @param end
-     *            last index of the unsorted portion of array
-     * @return index of the pivot
-     * @throws IOException
-     *             throws upon failure to acquire or write i/o sufficiently
-     */
-    private static int partition(BufferPool bp, int begin, int end)
-        throws IOException {
-        // variables to store data that is being swapped
-        byte[] pivot = new byte[4];
-        byte[] left = new byte[4];
-        byte[] right = new byte[4];
+		int i = begin - 1;
 
-        // loads the 4 byte record as a pivot used to sort the array
-        bp.getbytes(pivot, 4, end * 4);
-        short pivotKey = ByteBuffer.wrap(pivot).getShort();
+		for (int j = begin; j < end; j++) {
+			// loads the 4 byte record and extracts the short value from the
+			// first 2 that is used in sorting
+			bp.getbytes(left, 4, j * 4);
+			short key = ByteBuffer.wrap(left).getShort();
 
-        int i = begin - 1;
+			// this records belongs on the left side of the pivot
+			if (key <= pivotKey) {
+				i++;
 
-        for (int j = begin; j < end; j++) {
-            // loads the 4 byte record and extracts the short value from the
-            // first 2 that is used in sorting
-            bp.getbytes(left, 4, j * 4);
-            short key = ByteBuffer.wrap(left).getShort();
+				// Swaps the 4 bytes that begin at i and j
+				bp.getbytes(right, 4, i * 4);
+				bp.insert(left, 4, i * 4);
+				bp.insert(right, 4, j * 4);
+			}
+		}
+		// swaps the pivot the middle of the array
+		bp.getbytes(left, 4, (i + 1) * 4);
+		bp.insert(pivot, 4, (i + 1) * 4);
+		bp.insert(left, 4, end * 4);
 
-            // this records belongs on the left side of the pivot
-            if (key <= pivotKey) {
-                i++;
-
-                // Swaps the 4 bytes that begin at i and j
-                bp.getbytes(right, 4, i * 4);
-                bp.insert(left, 4, i * 4);
-                bp.insert(right, 4, j * 4);
-            }
-        }
-        // swaps the pivot the middle of the array
-        bp.getbytes(left, 4, (i + 1) * 4);
-        bp.insert(pivot, 4, (i + 1) * 4);
-        bp.insert(left, 4, end * 4);
-
-        // index of the pivot
-        return i + 1;
-    }
+		// index of the pivot
+		return i + 1;
+	}
 
 }
